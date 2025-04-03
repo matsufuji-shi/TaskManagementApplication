f1b2fb4680d1384e7b9e74abb6d1fb59affe04e3
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-03 15:32:50 +0900

    docs:cors文追加

---
+++ task-manager-backend/server.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const userRoutes = require("./routes/auth");
+
+app.use(express.json());
+app.use(cors());
+
+// ユーザー関連のAPIルートを使用
+app.use("/api", userRoutes);
+
+app.listen(3001, () => {
+    console.log('Server running on port 3001');
+});
+
