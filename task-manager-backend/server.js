c9e20584f724259a1fde3769b0e7b9c9b8224f88
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-03 17:45:10 +0900

    docs:db接続確認追加

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -1,7 +1,8 @@
+
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const db = require("./config/database");
+const app = express();
 
 const userRoutes = require("./routes/auth");  // 既存のユーザー認証ルート
 const tasksRouter = require("./routes/tasks"); // 追加するタスク管理ルート
@@ -9,10 +10,21 @@ const tasksRouter = require("./routes/tasks"); // 追加するタスク管理ル
 app.use(express.json());
 app.use(cors());
 
+// データベース接続確認
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error("データベース接続エラー:", err);
+        process.exit(1); // 接続失敗時にサーバを終了
+    } else {
+        console.log("データベース接続成功");
+        connection.release(); // 接続が成功したらリリース
+    }
+});
+
 // ユーザー関連のAPIルートを使用
 app.use("/api", userRoutes);
 
-//  タスク管理用のAPIルートを追加
+// タスク管理用のAPIルートを追加
 app.use("/api/tasks", tasksRouter);
 
 app.listen(3001, () => {
