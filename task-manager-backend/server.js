8f76308a5cc231014ba8c4629d7f980702cfd4d8
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-03 17:21:52 +0900

    docs:Server.jsの変更

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -1,8 +1,9 @@
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const userRoutes = require("./routes/auth");
+
+const userRoutes = require("./routes/auth");  // 既存のユーザー認証ルート
+const tasksRouter = require("./routes/tasks"); // 追加するタスク管理ルート
 
 app.use(express.json());
 app.use(cors());
@@ -10,7 +11,9 @@ app.use(cors());
 // ユーザー関連のAPIルートを使用
 app.use("/api", userRoutes);
 
+//  タスク管理用のAPIルートを追加
+app.use("/api/tasks", tasksRouter);
+
 app.listen(3001, () => {
     console.log('Server running on port 3001');
 });
-
