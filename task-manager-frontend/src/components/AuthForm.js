483e72654d04cd2d705ef1f562ea3f35a55e9b5a
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-04 18:33:09 +0900

    docs:コメントアウト削除

--- task-manager-frontend/src/components/AuthForm.js
+++ task-manager-frontend/src/components/AuthForm.js
@@ -9,49 +9,6 @@ function AuthForm({ setIsLoggedIn }) {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  // const handleSubmit = async (event, action) => {
-  //   event.preventDefault();
-
-  //   if (!username.trim()) {
-  //     setError("ユーザー名を入力してください");
-  //     return;
-  //   }
-  //   if (!password.trim()) {
-  //     setError("パスワードを入力してください");
-  //     return;
-  //   }
-  //   if (password.length < 6) {
-  //     setError("パスワードは6文字以上にしてください");
-  //     return;
-  //   }
-
-  //   setError("");
-  //   setLoading(true);
-
-  //   try {
-  //     let response;
-  //     if (action === "register") {
-  //       response = await axiosInstance.post("/register", { username, password });
-  //     } else if (action === "login") {
-  //       response = await axiosInstance.post("/login", { username, password });
-  //     }
-
-  //     console.log("レスポンス:", response.data);
-
-  //     if (response.data.token) {
-  //       // ログイン後にトークンをlocalStorageに保存
-  //       localStorage.setItem("token", response.data.token);
-  //       setIsLoggedIn(true); // ログイン状態を更新
-
-  //       // タスク一覧ページに遷移
-  //       navigate("/"); 
-  //     }
-  //   } catch (err) {
-  //     setError(err.response?.data?.message || "エラーが発生しました");
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
 
   const handleSubmit = async (event, action) => {
     event.preventDefault();
