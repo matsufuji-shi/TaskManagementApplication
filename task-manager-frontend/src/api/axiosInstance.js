b0ebf0c8520fe5cc9e0528dc7359c8cb6542164b
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-08 15:40:38 +0900

    docs:コメント変更

--- task-manager-frontend/src/api/axiosInstance.js
+++ task-manager-frontend/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+//バックエンドとのAPI通信
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api", //バックエンドのAPIのURL
+  baseURL: "http://localhost:3001/api", 
   headers: {
     "Content-Type": "application/json",
   },
